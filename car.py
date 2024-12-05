177b2f1854159479a852ed1fe8c9a11cd6c199cd
sital-tharu <sitatharu769@gmail.com>
2024-12-05 14:07:31 +0530

    using plt to show dat

--- car.py
+++ car.py
@@ -1,6 +1,7 @@
 
 import kagglehub
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Download latest version
 car_data = kagglehub.dataset_download("nehalbirla/vehicle-dataset-from-cardekho")
@@ -13,11 +14,13 @@ print("Path to dataset files:", car_data)
 file_path = car_data + '/Car data.csv' 
 data = pd.read_csv(file_path) 
 print(data)
-# using ui to help me pick a perticular data 
+# using ai to help me pick a perticular data 
 # Filter the DataFrame to show only cars with a selling price >= 5 lakhs
-print(data[data['Selling_Price'] >= 5.00])
+#print(data[data['Selling_Price'] >= 5.02])
+print(data[data["Year"] == 2014])
 
-# Display the filtered DataFrame
-
-
-
+plt.scatter(data["Year"], data["Selling_Price"])
+plt.title("Data set from Kaggel")
+plt.xlabel("Year ")
+plt.ylabel("Selling price")
+plt.show()
